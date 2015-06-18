158cb6732bb96f635fa7e26da2015d9bb41f5968
deostroll <deostroll@gmail.com>
2015-06-18 23:48:15 +0530

    viewutils.js: - - created frame object. Manages dirty checking and cleanup
    pages.json:
    -
    - added example for a blank page

--- app/scripts/viewutils.js
+++ app/scripts/viewutils.js
@@ -2,21 +2,52 @@ angular.module('view.utils',[])
 	.directive('ngFrame', function($compile, $controller, $templateRequest, $sce, $parse){
 		var postLinkFn = function(scope, elem, attrs) {
 			var srcExp = attrs.src;			
-			
-			scope.$watch($sce.parseAsResourceUrl(srcExp), function srcWatch(src){
-				elem.empty();
-				var ctrlExp = $parse(attrs.controller)(scope);
+			//begin 1: one and only frame element
+			var frame = {
+				$isDirty : function() {
+					return !!this.$scope;
+				},
+				$setDirty: function(scope) {
+					this.$scope = scope;
+				},
+				$cleanup: function(){
+					if(this.$scope) {
+						this.$scope.$destroy();
+						this.$scope = null;
+					}
+				}
+
+			}
+			//end 1: frame element			
+
+			scope.$watch($sce.parseAsResourceUrl(srcExp), function srcWatch(src) {
+				
 				if(src) {
-					var newScope = scope.$new();
-					$controller(ctrlExp, {'$scope' : newScope });
-					$templateRequest(src).then(function(tmpl){
+
+					if(frame.$isDirty()) {
+						frame.$cleanup();
+						elem.empty();
+					}
+
+					$templateRequest(src).then(function(tmpl) {
+						var newScope = scope.$new();
+						var ctrlExp = $parse(attrs.controller)(scope);
+						$controller(ctrlExp, {'$scope' : newScope });
 						var el = $compile(tmpl)(newScope);
-						//console.log(el);
 						elem.append(el);
+						frame.$setDirty(newScope);
 					});
+				}
+				else {
+					if(frame.$isDirty()) {
+						frame.$cleanup();
+						elem.empty();
+					}					
 				}
 			});
 		};
+
+		//directive api
 		return {
 			link: postLinkFn
 		};
