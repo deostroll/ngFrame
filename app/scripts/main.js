3c3cf09083eb811155cdb8ea7779bff6335811c6
arun <arun_jayapal@infosys.com>
2015-06-19 11:17:16 +0530

    index.html: 	1. changed ngFrame src and controller attribute
    main.js:
    	1: AppCtrl
    		1.1: added new scope variable 'example'
    		1.2: added watch to watch on scope variable 'demo'
    pages.json:
    	1: added example of blank page
    	2: added example to show scope events
    
    viewutils.js:
    	1: refactored code
    		1.1: `locals` variable - to store values essential to
    		ngFrame page loading
    		1.2: pageChangeFn - function to take care of new page load
    		1.3: srcWatchFn - ngFrame src $watch handler

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,14 +3,22 @@ angular.module('myapp', ['view.utils', 'ngMaterial'])
 		
 		$scope.demo = null;
 		$scope.demos = null;
+		$scope.example = {};
+
 		$http({url: 'scripts/pages.json'})
 			.success(function(data){
 				$scope.demos = data;
 			});
+
+		$scope.$watch('demo', function(demo) {
+			$scope.example = angular.extend({}, demo);
+		});
 	})
 	.controller('FruitsCtrl', function($scope){
 		$scope.fruits = ['grapes', 'banannas', 'oranges', 'water melon', 'guava', 'cherry'];
 	})
 	.controller('TwoWayCtrl', function($scope){
-
+		$scope.$on('$destroy', function() {
+			console.log("$scope destroy on TwoWayCtrl called");
+		});
 	});
--- app/scripts/viewutils.js
+++ app/scripts/viewutils.js
@@ -1,16 +1,25 @@
 angular.module('view.utils',[])
 	.directive('ngFrame', function($compile, $controller, $templateRequest, $sce, $parse){
 		var postLinkFn = function(scope, elem, attrs) {
-			var srcExp = attrs.src;			
-			//begin 1: one and only frame element
+			var srcExp = attrs.src;		
+			
+			//locals
+			var locals = {
+				src: null,
+				controller: null,
+				srcExp: $parse(attrs.src),
+				ctrlExp: $parse(attrs.controller)
+			};
+
+			//begin 1: frame: interface to manage frame
 			var frame = {
-				$isDirty : function() {
+				$$isDirty : function() {
 					return !!this.$scope;
 				},
-				$setDirty: function(scope) {
+				$$setDirty: function(scope) {
 					this.$scope = scope;
 				},
-				$cleanup: function(){
+				$$cleanup: function(){
 					if(this.$scope) {
 						this.$scope.$destroy();
 						this.$scope = null;
@@ -20,31 +29,41 @@ angular.module('view.utils',[])
 			}
 			//end 1: frame element			
 
-			scope.$watch($sce.parseAsResourceUrl(srcExp), function srcWatch(src) {
-				
+			//begin 2: pageChangeFn: loads new page in ngFrame
+			var pageChangeFn = function pageChangeFn(src) {
 				if(src) {
 
-					if(frame.$isDirty()) {
-						frame.$cleanup();
+					if(frame.$$isDirty()) {
+						frame.$$cleanup();
 						elem.empty();
 					}
 
 					$templateRequest(src).then(function(tmpl) {
 						var newScope = scope.$new();
-						var ctrlExp = $parse(attrs.controller)(scope);
+						var ctrlExp = locals.controller;
 						$controller(ctrlExp, {'$scope' : newScope });
 						var el = $compile(tmpl)(newScope);
 						elem.append(el);
-						frame.$setDirty(newScope);
+						frame.$$setDirty(newScope);
 					});
 				}
 				else {
-					if(frame.$isDirty()) {
-						frame.$cleanup();
+					if(frame.$$isDirty()) {
+						frame.$$cleanup();
 						elem.empty();
 					}					
 				}
-			});
+			};
+			//end 2: pageChangeFn
+
+			//srcWatchFn
+			var srcWatchFn = function srcWatchFn(src) {
+				locals.src = src;
+				locals.controller = locals.ctrlExp(scope);
+				pageChangeFn(src);
+			};
+
+			scope.$watch($sce.parseAsResourceUrl(srcExp), srcWatchFn);
 		};
 
 		//directive api
